474c24b8be502da28567749d4ee881ba6bf75473
Miquel Puig <mpuig22@gmail.com>
2019-03-18 18:21:01 +0100

    datos a medias

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -45,7 +45,9 @@ export class Tab1Page {
               if ( data.titulo.length === 0 ) {
                 return;
               }
-              this.deseosService.crearLista(data.titulo);
+              const listaId = this.deseosService.crearLista(data.titulo);
+
+              this.router.navigateByUrl(`/tabs/tab1/agregar/${ listaId }`);
            }
 
          }
