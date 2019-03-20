481c92041f91903eb3d94bf2c3035ff852190c75
Miquel Puig Gibert <miquel@MacBook-Pro.home>
2019-03-20 22:21:21 +0100

    previo al borrado de items

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -33,6 +33,22 @@ export class AgregarPage implements OnInit {
     this.lista.items.push( nuevoItem );
 
     this.nombreItem = '';
+    this.deseosService.guardarStorage();
+  }
+
+  cambioCheck( item: ListaItem ) {
+    const pendientes = this.lista.items.filter (itemData => {
+      return itemData.completado === false;
+    }).length;
+    //console.log({pendientes});
+    if( pendientes ===0) {
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    } else {
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+    this.deseosService.guardarStorage();
   }
 
 }
